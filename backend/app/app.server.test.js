b9b010878bcdc69ad14359db3500f0a0a718a858
Rogerio Angeliski <rogerio.angeliski@resultadosdigitais.com.br>
2020-04-09 22:53:45 -0300

    Add server to test

--- backend/app/app.server.test.js
+++ backend/app/app.server.test.js
@@ -2,19 +2,27 @@ const request = require("supertest");
 const app = require("../app/app.server");
 
 describe("Unit testing the / route", () => {
-  it("should return OK status", () => request(app)
+  let server;
+  beforeEach(() => {
+    server = app.listen();
+  });
+  afterEach(() => {
+    server.close();
+  });
+
+  it("should return OK status", () => request(server)
     .get("/")
     .then((response) => {
       expect(response.status).toEqual(200);
     }));
 
-  it("should there is an element with id='application' to render react app", () => request(app)
+  it("should there is an element with id='application' to render react app", () => request(server)
     .get("/")
     .then((response) => {
       expect(response.text).toContain("<div id=\"application\"></div>");
     }));
 
-  it("should there is login.js on rendering", () => request(app)
+  it("should there is login.js on rendering", () => request(server)
     .get("/")
     .then((response) => {
       expect(response.text).toMatch(
